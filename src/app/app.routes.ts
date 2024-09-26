ea6e956cf87f0b2c4d04540c37b98026daa1e104
Dafnik <git@dafnik.me>
2024-09-26 14:09:10 +0200

    Removed default template except router outlet, added todos component and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full',
+  },
+  {
+    path: 'todos',
+    loadComponent: () => import('./todos/todos.component').then((c) => c.TodosComponent),
+  },
+];
